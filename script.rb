84768382ca96c3951dc49fd5947cd2b39f0b4b67
Lucas Pedroza <P.lucas021@gmail.com>
2017-01-04 00:48:52 +0000

    mail

--- script.rb
+++ script.rb
@@ -14,7 +14,7 @@ class Application < Sinatra::Base
       :via_options => {
         :address => 'smtp.sendgrid.net',
         :port => '587',
-        :domain => 'young-beach-14460.herokuapp.com',
+        :domain => ENV['SENDGRID_DOMAIN'],
         :user_name => ENV['SENDGRID_USERNAME'],
         :password => ENV['SENDGRID_PASSWORD'],
         :authentication => :plain,
